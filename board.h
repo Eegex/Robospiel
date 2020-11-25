d804d26f14f446ecc34d27b9f9ab365c9007f2ea
Jan Niemeier <jn@ipe-bielefeld.de>
2020-11-25 21:09:50 +0100

    seed fix

--- board.h
+++ board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 
 #include <QObject>
+#include <QTime>
 #include <QSize>
 #include "tile.h"
 
@@ -12,8 +13,8 @@ class Board : public QObject
 public:
 	Board(QObject *parent, int width, int height, int playerNumber);
 	Tile *getTile(int x, int y);
-    QSize getSize();
-    std::string printDirection(Direction direction);
+	QSize getSize();
+	std::string printDirection(Direction direction);
 private:
 	QVector<QVector<Tile*>> tiles;
 	QVector<Tile*> players;
@@ -21,20 +22,18 @@ private:
 	//TODO history
 	//TODO active user
 
-    Tile *getRandomUnoccupiedTile();
-    // for each side: -1 because there are more tiles than walls, -2 because the walls next to the sides shouldn't be set, so 3*4 = 12
-    const int NUM_PLACES_THAT_CANT_HAVE_OUTER_WALLS = 12;
-    const int NO_OUTER_WALLS_PER_SIDE = 3;
-    const int SPREAD_FACTOR_INNER_WALLS = 10;
+	Tile *getRandomUnoccupiedTile();
+	// for each side: -1 because there are more tiles than walls, -2 because the walls next to the sides shouldn't be set, so 3*4 = 12
+	const int NUM_PLACES_THAT_CANT_HAVE_OUTER_WALLS = 12;
+	const int SPREAD_FACTOR_INNER_WALLS = 15;
 
-    const int AVG_DIST_OF_OUTER_WALLS = 4;
-    const int SPREAD_FACTOR_OF_OUTER_WALLS = 2;
-    void placeInnerWalls();
-    void placeOuterWalls();
-    bool placeInnerWallifFits(Tile *tile, Direction direection);
-    Direction getNextDirection(Direction direction, int numberOfClockwiseSteps);
-    bool placeOuterWallIfFits(Tile *, Direction direction);
-    void placeGoalInCorner();
+	const int AVG_DIST_OF_OUTER_WALLS = 4;
+	void placeInnerWalls();
+	void placeOuterWalls();
+	bool placeInnerWallifFits(Tile *tile, Direction direection);
+	Direction getNextDirection(Direction direction, int numberOfClockwiseSteps);
+	bool placeOuterWallIfFits(Tile *, Direction direction);
+	void placeGoalInCorner();
 };
 
 #endif // BOARD_H
