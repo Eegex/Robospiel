f7c6d67d4c191e65cefe3d10080bbabef4ce7b8f
NoraHille <norahille@web.de>
2021-04-14 22:23:45 +0200

    master runs

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -7,7 +7,7 @@ MainWidget::MainWidget(QWidget *parent) : QWidget(parent)
 	glMain = new QGridLayout(this);
 	leaderboard = new LeaderBoardWidget(this);
 	skipBtn = new QPushButton("Skip", this);
-    initializeView(GameControll::setBoard(new Board(5, 5, 24)), GameControll::getMapping());
+    initializeView(GameControll::setBoard(new Board(10, 10, 5)), GameControll::getMapping());
 	//connect(view, &BoardView::lastAnimationAfterGoalHitEnded, game, &GameControll::calculateWinner);
 	networkView = new NetworkView;
 	lcd = new QLCDNumber(this);
