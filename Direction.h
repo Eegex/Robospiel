30cd891e42757276cbdf7bd79392d0a0ea5216b4
dbrohl <dbrohl@techfak.uni-bielefeld.de>
2021-06-29 23:28:57 +0200

    different values for PlayerAction enum

--- Direction.h
+++ Direction.h
@@ -8,14 +8,15 @@
 enum class Direction{none = 0x0, north = 0x1, east = 0x2, south = 0x4, west = 0x8};
 
 //add string representation to networkmodel.cpp
-enum PlayerAction{none = 0x0, movement = 0x10, movePlayerNorth = 0x11,movePlayerEast = 0x12,movePlayerSouth = 0x14,movePlayerWest = 0x18,
-				  playerSwitch = 0x20, switchPlayerNorth = 0x21,switchPlayerEast = 0x22,switchPlayerSouth = 0x24,switchPlayerWest = 0x28,
-				  bidding = 0x40, enterBidding = 0x41, sendBidding = 0x42, clearBidding = 0x44, skipTimer = 0x48,
-				  other = 0x80, giveUp = 0x81, revert = 0x82, revertToBeginning = 0x84, freePlay = 0x88,
-                  user = 0x100, newUser = 0x101, changedUsername = 0x102, changedUserColor = 0x104, userLeft=0x108,
-				  update = 0x200, completeUpdate = 0x201, boardUpdate = 0x202,
-				  menuAction = 0x400, editBoard = 0x401, nextTarget = 0x402, setIdle = 0x403,
-				  intern = 0x1000, syncRandomGenerators = 0x1001, registerClient = 0x1002
+enum PlayerAction{none = 0x0,
+                  movement = 0x100, movePlayerNorth = 0x101,movePlayerEast = 0x102,movePlayerSouth = 0x104,movePlayerWest = 0x108,
+                  playerSwitch = 0x200, switchPlayerNorth = 0x201,switchPlayerEast = 0x202,switchPlayerSouth = 0x204,switchPlayerWest = 0x208,
+                  bidding = 0x400, enterBidding = 0x401, sendBidding = 0x402, clearBidding = 0x404,
+                  other = 0x800, giveUp = 0x801, revert = 0x802, revertToBeginning = 0x804, freePlay = 0x808, skipTimer = 0x810,
+                  user = 0x1000, newUser = 0x1001, changedUsername = 0x1002, changedUserColor = 0x1004, userLeft=0x1008,
+                  update = 0x2000, completeUpdate = 0x2001, boardUpdate = 0x2002,
+                  menuAction = 0x4000, editBoard = 0x4001, nextTarget = 0x4002, setIdle = 0x4003,
+                  intern = 0x8000, syncRandomGenerators = 0x8001, registerClient = 0x8002
 				 };
 
 //the Intern subspace should be the end of this enum
